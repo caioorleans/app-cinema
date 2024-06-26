c79680dbf2c4288c787076cc8557b4aca24534c6
douglas2569 <carlosdouglas@alu.ufc.br>
2024-06-26 09:05:25 -0300

    update home
    update homescreen

--- app/src/main/java/com/example/cinema/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/cinema/ui/screens/home/HomeScreen.kt
@@ -6,16 +6,25 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.cinema.R
@@ -27,39 +36,44 @@ import com.example.cinema.ui.theme.Secondary
 @Composable
 fun HomeScreen(modifier:Modifier = Modifier) {
     Surface(modifier = modifier.fillMaxSize().padding( horizontal = 8.dp).padding(top = 16.dp)) {
-        val scrollState = rememberScrollState()
-
-        Column(modifier = modifier
-            .verticalScroll(scrollState)
-                .fillMaxSize()
-                .background(Secondary)
-        ) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-
-
-                ItemCardMovie()
-                ItemCardMovie()
-
-
-            }
+                    LazyVerticalGridCinema()
         }
     }
+
+@Composable
+fun LazyVerticalGridCinema(){
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(128.dp),
+        modifier = Modifier.fillMaxSize().background(Secondary),
+
+        content = {
+            item { ItemCardMovie() }
+            item { ItemCardMovie() }
+            item { ItemCardMovie() }
+            item { ItemCardMovie() }
+            item { ItemCardMovie() }
+        }
+    )
 }
+
 
 @Composable
 fun ItemCardMovie() {
     Box(
         modifier = Modifier
-            .padding(4.dp)
+            .padding(6.dp)
+            .height(300.dp)
+            .clip(RoundedCornerShape(16.dp))
+        ,
+
     ) {
        Image(
-           painter =
-           painterResource(id = R.drawable.ic_launcher_background),
-           contentDescription = "")
+           contentScale = ContentScale.FillBounds,
+           modifier = Modifier.fillMaxSize(),
+           painter = painterResource(id = R.drawable.ic_launcher_background),
+           contentDescription = "",
+
+           )
+
     }
 }
-
-
